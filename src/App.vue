c332ab0a66e4000e00aab148227c48246471f2ba
uy_sun <hmy0119@gmail.com>
2023-06-26 11:23:58 +0800

    添加主题简陋的切换按钮

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <script setup lang="ts">
-import { reactive } from "vue";
+import { reactive, provide, ref } from "vue";
 
 import axios from "axios";
+import type { GlobalTheme } from "naive-ui";
+import { NConfigProvider, NCard, NBackTop } from "naive-ui";
 
 import { Results } from "@/types/results";
 
 import GithubButton from "./components/GithubButton.vue";
 import ResultTable from "./components/ResultTable.vue";
+import ThemeSwitcher from "./components/ThemeSwitcher.vue";
+
 const state = reactive({
   results: {} as Results,
+  theme: ref<GlobalTheme | null>(null),
 });
 
 axios
@@ -19,11 +24,29 @@ axios
   .catch((error) => {
     console.error(error);
   });
+
+function setTheme(value: GlobalTheme | null) {
+  console.log("setTheme", value);
+  state.theme = value;
+}
+
+provide("theme", {
+  theme: state.theme,
+  setTheme: setTheme,
+});
 </script>
 
 <template>
-  <GithubButton />
-  <ResultTable :results="state.results" />
+  <div class="container">
+    <n-config-provider :theme="state.theme">
+      <n-card>
+        <GithubButton />
+        <ThemeSwitcher />
+        <ResultTable :results="state.results" />
+      </n-card>
+      <n-back-top :right="100" />
+    </n-config-provider>
+  </div>
 </template>
 
 <style scoped></style>
---
+++ src/components/ThemeSwitcher.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { inject } from "vue";
+
+import type { GlobalTheme } from "naive-ui";
+import { NButton, darkTheme, lightTheme } from "naive-ui";
+
+const { setTheme } = inject("theme") as {
+  setTheme: (value: GlobalTheme | null) => void;
+};
+</script>
+
+<template>
+  <n-button @click="setTheme(darkTheme)"> 深色 </n-button>
+  <n-button @click="setTheme(lightTheme)"> 浅色 </n-button>
+</template>
