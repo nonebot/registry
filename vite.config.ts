df6d1c12b4f4c2a7e9fe2ea88fc081b93e96f208
StarHeartHunt <starheart233@gmail.com>
2023-06-26 23:06:24 +0800

    build: add build inspector and manual chunk config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import path from "path";
 
 import vue from "@vitejs/plugin-vue";
+import { visualizer } from "rollup-plugin-visualizer";
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS()],
+  plugins: [vue(), UnoCSS(), visualizer({ sourcemap: true })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -17,6 +18,19 @@ export default defineConfig({
       "/results.json": {
         target: "https://registry.nonebot.dev",
         changeOrigin: true,
+      },
+    },
+  },
+  build: {
+    sourcemap: true,
+
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("naive-ui")) return "naive-ui";
+          if (id.includes("highlight.js")) return "highlight-js";
+          if (id.includes("node_modules")) return "vendor";
+        },
       },
     },
   },
