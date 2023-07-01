8ef91287073b049c1b02542391ea469e28cc415e
StarHeartHunt <starheart233@gmail.com>
2023-07-01 16:23:04 +0800

    chore(deps): add unocss directives transformer and bump deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from "path";
 
+import transformerDirectives from "@unocss/transformer-directives";
 import vue from "@vitejs/plugin-vue";
 import { visualizer } from "rollup-plugin-visualizer";
 import UnoCSS from "unocss/vite";
@@ -7,7 +8,13 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), UnoCSS(), visualizer({ sourcemap: true })],
+  plugins: [
+    vue(),
+    UnoCSS({
+      transformers: [transformerDirectives()],
+    }),
+    visualizer({ sourcemap: true }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
