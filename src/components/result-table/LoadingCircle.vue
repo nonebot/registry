07769df42ba9e240f617d1a97cc1406f197fc480
StarHeartHunt <starheart233@gmail.com>
2023-07-02 17:40:41 +0800

    fix: add margin for skeletons

--- src/components/result-table/LoadingCircle.vue
+++ src/components/result-table/LoadingCircle.vue
@@ -4,6 +4,6 @@ import { NSkeleton, NSpace } from "naive-ui";
 
 <template>
   <n-space justify="center">
-    <n-skeleton size="small" circle />
+    <n-skeleton class="mr-[15px]" size="small" circle />
   </n-space>
 </template>
