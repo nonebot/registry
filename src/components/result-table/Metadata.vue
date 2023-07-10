a3fa7df2b48a51a8529ddd7bfc6d0a74eb9bf764
StarHeartHunt <starheart233@gmail.com>
2023-07-10 09:53:42 +0800

    chore: revert Metadata change

--- src/components/result-table/Metadata.vue
+++ src/components/result-table/Metadata.vue
@@ -1,53 +1,24 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { NCode } from "naive-ui";
 
-import hljsVuePlugin from "@highlightjs/vue-plugin";
-import hljs from "highlight.js/lib/core";
-import json from "highlight.js/lib/languages/json";
-import { NModal } from "naive-ui";
-import PackageVariant from "vue-material-design-icons/PackageVariant.vue";
-import PackageVariantRemove from "vue-material-design-icons/PackageVariantRemove.vue";
-
-import { Results } from "@/types/results";
-
-hljs.registerLanguage("json", json);
+import type { Results } from "@/types/results";
 
 const props = defineProps<{
-  projectLink: string;
   result: Results[keyof Results];
+  dense?: boolean;
 }>();
-const showModal = ref(false);
 </script>
 
 <template>
-  <div
-    class="mr-[15px] flex justify-center align-middle cursor-pointer"
-    @click="showModal = true"
-  >
-    <PackageVariant
-      v-if="result.results.metadata"
-      class="flex justify-center align-middle"
-      style="color: #5784ff"
-    />
-    <PackageVariantRemove
-      v-else
-      class="flex justify-center align-middle"
-      style="color: #ffcc66"
-    />
-  </div>
-  <n-modal
-    v-model:show="showModal"
-    class="max-w-3/4"
-    preset="card"
-    :title="`${projectLink} 元数据`"
-  >
-    <template #default>
-      <hljsVuePlugin.component
-        class="max-h-[50vh] overflow-auto"
-        language="json"
-        :autodetect="false"
-        :code="JSON.stringify(props.result.outputs.metadata, null, 2)"
-      />
-    </template>
-  </n-modal>
+  <n-code
+    class="overflow-auto"
+    :class="{
+      'max-h-[50vh]': dense,
+    }"
+    :code="JSON.stringify(props.result.outputs.metadata, null, 2)"
+    language="json"
+    word-wrap
+  />
 </template>
+
+<style scoped></style>
