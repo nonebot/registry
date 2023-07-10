f13322e27a41deec3790ffc2b4d1c44135db65de
Azide <rukuy@qq.com>
2023-07-10 22:42:42 +0800

    style: 稍微调小详情按钮

--- src/components/result-table/Detail.vue
+++ src/components/result-table/Detail.vue
@@ -6,6 +6,6 @@ defineProps<{ pypi: string; module: string }>();
 
 <template>
   <router-link :to="`/plugin/${pypi}:${module}`">
-    <n-button> 详情 </n-button>
+    <n-button size="tiny"> 详情 </n-button>
   </router-link>
 </template>
