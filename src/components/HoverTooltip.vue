2a8c0d8a20b91c84a367055460d5f70a6b0366b2
StarHeartHunt <starheart233@gmail.com>
2023-09-09 16:59:24 +0800

    refactor: cleanup unused layout

---
+++ src/components/HoverTooltip.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { NTooltip } from "naive-ui";
+
+defineProps<{
+  source: string;
+  tip: string;
+}>();
+</script>
+
+<template>
+  <n-tooltip trigger="hover">
+    <template #trigger>
+      {{ source }}
+    </template>
+    {{ tip }}
+  </n-tooltip>
+</template>
+
+<style scoped></style>
--- src/views/PluginPage.vue
+++ src/views/PluginPage.vue
@@ -3,20 +3,18 @@ import { computed } from "vue";
 
 import {
   NA,
+  NAlert,
   NButton,
-  NIcon,
-  NText,
   NCard,
   NH3,
+  NIcon,
   NLayout,
   NP,
   NPageHeader,
   NResult,
   NSkeleton,
-  NTabPane,
-  NTabs,
   NTag,
-  NAlert,
+  NText,
 } from "naive-ui";
 import { storeToRefs } from "pinia";
 import CheckCircle from "vue-material-design-icons/CheckCircle.vue";
@@ -28,6 +26,7 @@ import Load from "@/components/result-table/Load.vue";
 import Metadata from "@/components/result-table/Metadata.vue";
 import Validation from "@/components/result-table/Validation.vue";
 import { usePageStore } from "@/stores/page";
+import { pickTextColor } from "@/utils/color";
 import NotFound from "@/views/NotFound.vue";
 
 const props = defineProps<{ path: string }>();
@@ -42,14 +41,6 @@ const [pypi, module] = props.path
 
 const plugin = computed(() => !loading.value && store.getPlugin(pypi, module));
 const result = computed(() => !loading.value && store.getResult(pypi, module));
-
-function pickTextColor(bgColor: string): string {
-  let color = bgColor.charAt(0) === "#" ? bgColor.substring(1, 7) : bgColor;
-  let r = parseInt(color.substring(0, 2), 16); // hexToR
-  let g = parseInt(color.substring(2, 4), 16); // hexToG
-  let b = parseInt(color.substring(4, 6), 16); // hexToB
-  return r * 0.299 + g * 0.587 + b * 0.114 > 186 ? "#000" : "#fff";
-}
 </script>
 
 <template>
@@ -64,110 +55,105 @@ function pickTextColor(bgColor: string): string {
         </h2>
       </template>
     </n-page-header>
-    <n-tabs animated>
-      <n-tab-pane name="overview" tab="总览">
-        <div class="flex flex-col-reverse xl:flex-row justify-between">
-          <div class="xl:basis-3/4 xl:max-w-3/4 xl:pr-2">
-            <n-p>
-              <n-h3>验证结果</n-h3>
-              <Validation
-                v-if="result.outputs.validation"
-                :validation="result.outputs.validation"
-                :plugin="plugin"
-                class="w-4/5"
-              />
-              <n-alert
-                v-else-if="plugin.skip_test"
-                title="验证通过"
-                type="info"
-                class="w-220px"
-              >
-                <template #icon>
-                  <n-icon>
-                    <RocketLaunch />
-                  </n-icon>
-                </template>
-                <n-text>该插件经人工跳过了测试</n-text>
-              </n-alert>
-              <n-alert v-else title="验证通过" type="success" class="w-140px">
-                <template #icon>
-                  <n-icon>
-                    <RocketLaunch />
-                  </n-icon>
-                </template>
-              </n-alert>
-            </n-p>
-            <n-p>
-              <n-h3>元数据</n-h3>
-              <Metadata :result="result" class="w-4/5" />
-            </n-p>
-            <n-p
-              ><n-h3>加载结果</n-h3>
-              <Load :result="result" />
-            </n-p>
-          </div>
-          <div class="pb-4 xl:ml-4 xl:basis-1/4 xl:max-w-1/4 xl:pl-2">
-            <n-h3>插件信息</n-h3>
-            <n-p> 作者：<Author :author="plugin.author" /> </n-p>
-            <n-p> 版本：{{ plugin.version }} </n-p>
-            <n-p> 描述：{{ plugin.desc }} </n-p>
-            <n-p>
-              标签：<n-tag
-                v-for="(tag, index) in plugin.tags"
-                :key="index"
-                class="mr-1 mb-1"
-                :bordered="false"
-                :color="{
-                  color: tag.color,
-                  textColor: pickTextColor(tag.color),
-                }"
-              >
-                {{ tag.label }}
-              </n-tag>
-              <n-button v-if="plugin.tags.length === 0" text>无</n-button>
-            </n-p>
-            <n-p> 类型：{{ plugin.type || "未标记" }} </n-p>
-            <n-p
-              >pypi：
-              <n-a
-                class="text-inherit hover:color-[#ea5252] no-underline"
-                :href="`https://pypi.org/project/${plugin.project_link}/`"
-                target="_blank"
-              >
-                {{ plugin.project_link }}
-              </n-a>
-            </n-p>
-            <n-p
-              >主页：
-              <n-a
-                class="text-inherit hover:color-[#ea5252] no-underline"
-                :href="plugin.homepage"
-                target="_blank"
-              >
-                {{ plugin.module_name }}
-              </n-a>
-            </n-p>
-            <n-p
-              >适配器支持：
-              <n-tag
-                v-for="(adapter, index) in plugin.supported_adapters || [
-                  '所有/未标记',
-                ]"
-                :key="index"
-                class="mr-1 mb-1"
-                type="success"
-              >
-                {{ adapter.replace("nonebot.adapters.", "") }}
-              </n-tag>
-            </n-p>
-            <n-p
-              >验证时间：
-              {{ new Date(plugin.time).toLocaleString() }}
-            </n-p>
-          </div>
-        </div>
-      </n-tab-pane>
-    </n-tabs>
+    <div class="flex flex-col-reverse xl:flex-row justify-between">
+      <div class="xl:basis-3/4 xl:max-w-3/4 xl:pr-2">
+        <n-p>
+          <n-h3>验证结果</n-h3>
+          <Validation
+            v-if="result.outputs.validation"
+            :validation="result.outputs.validation"
+            :plugin="plugin"
+          />
+          <n-alert
+            v-else-if="plugin.skip_test"
+            title="验证通过"
+            type="info"
+            class="w-220px"
+          >
+            <template #icon>
+              <n-icon>
+                <RocketLaunch />
+              </n-icon>
+            </template>
+            <n-text>该插件经人工跳过了测试</n-text>
+          </n-alert>
+          <n-alert v-else title="验证通过" type="success" class="w-140px">
+            <template #icon>
+              <n-icon>
+                <RocketLaunch />
+              </n-icon>
+            </template>
+          </n-alert>
+        </n-p>
+        <n-p>
+          <n-h3>元数据</n-h3>
+          <Metadata :result="result" />
+        </n-p>
+        <n-p
+          ><n-h3>加载结果</n-h3>
+          <Load :result="result" />
+        </n-p>
+      </div>
+      <div class="pb-4 xl:ml-4 xl:basis-1/4 xl:max-w-1/4 xl:pl-2">
+        <n-h3>插件信息</n-h3>
+        <n-p> 作者：<Author :author="plugin.author" /> </n-p>
+        <n-p> 版本：{{ plugin.version }} </n-p>
+        <n-p> 描述：{{ plugin.desc }} </n-p>
+        <n-p>
+          标签：<n-tag
+            v-for="(tag, index) in plugin.tags"
+            :key="index"
+            class="mr-1 mb-1"
+            :bordered="false"
+            :color="{
+              color: tag.color,
+              textColor: pickTextColor(tag.color),
+            }"
+          >
+            {{ tag.label }}
+          </n-tag>
+          <n-button v-if="plugin.tags.length === 0" text>无</n-button>
+        </n-p>
+        <n-p> 类型：{{ plugin.type || "未标记" }} </n-p>
+        <n-p
+          >pypi：
+          <n-a
+            class="text-inherit hover:color-[#ea5252] no-underline"
+            :href="`https://pypi.org/project/${plugin.project_link}/`"
+            target="_blank"
+          >
+            {{ plugin.project_link }}
+          </n-a>
+        </n-p>
+        <n-p
+          >主页：
+          <n-a
+            class="text-inherit hover:color-[#ea5252] no-underline"
+            :href="plugin.homepage"
+            target="_blank"
+          >
+            {{ plugin.module_name }}
+          </n-a>
+        </n-p>
+        <n-p
+          >适配器支持：
+          <n-tag
+            v-for="(adapter, index) in plugin.supported_adapters || [
+              '所有/未标记',
+            ]"
+            :key="index"
+            class="mr-1 mb-1"
+            type="success"
+          >
+            {{ adapter.replace("nonebot.adapters.", "") }}
+          </n-tag>
+        </n-p>
+        <n-p
+          >验证时间：
+          {{ new Date(plugin.time).toLocaleString() }}
+        </n-p>
+      </div>
+    </div>
   </div>
   <div v-else-if="pypi && module">
     <n-layout class="m-3">
