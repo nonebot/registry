ff7486534f20cfdfa271ad3c504879ae43a6b677
StarHeartHunt <starheart233@gmail.com>
2023-09-10 12:51:26 +0800

    fix: fix icon center in stat

--- src/components/statistic/Details.vue
+++ src/components/statistic/Details.vue
@@ -11,22 +11,26 @@ defineProps<{ loadCount: number; metadataCount: number }>();
   <n-space justify="space-around" inline>
     <div class="w-[50px] inline-flex items-center align-middle">
       <Icon
-        size="16"
+        size="14"
         color="#66afd3"
         class="mr-1 overflow-hidden"
         :path="mdiPackageVariantClosedCheck"
       />
-      <n-number-animation :from="0" :to="metadataCount" />
+      <span class="inline-flex items-center align-middle text-sm!">
+        <n-number-animation class="text-xs!" :from="0" :to="metadataCount" />
+      </span>
     </div>
     <n-divider vertical />
     <div class="w-[50px] inline-flex items-center align-middle">
       <Icon
-        size="16"
+        size="14"
         color="#5acea7"
         class="mr-1 overflow-hidden"
         :path="mdiPuzzleCheckOutline"
       />
-      <n-number-animation :from="0" :to="loadCount" />
+      <span class="inline-flex items-center align-middle text-sm!">
+        <n-number-animation :from="0" :to="loadCount" />
+      </span>
     </div>
   </n-space>
 </template>
